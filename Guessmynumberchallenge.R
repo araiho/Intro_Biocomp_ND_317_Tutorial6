3c9cad598e7afa8c5a109ab0c35a8eddcf72bd4a
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-10-04 12:23:46 -0400

    Issues resolved when running in source

--- Guessmynumberchallenge.R
+++ Guessmynumberchallenge.R
@@ -1,24 +1,26 @@
+#Note: code is designed to work from source and not interactively
+
 rm(list=ls())
 
 #make a vector of numbers from 1 to 100
 v=1:100
-#take input from the user-guess
-guess=100000
+
 #thinking of a number
 number= sample(v,size = 1,replace = FALSE, prob = NULL)
 
 print("I'm thinking of a number 1-100...")
 
+#take input from the user-guess
+guess=readline(prompt = 'Guess: ')
+
 while(guess != number){
-  if (guess == 100000){
-    print("Make a guess")
-  }
-  else if(guess < number){
+  if(guess < number){
      print("Higher!")
   } else{
   print("Lower!")}
   guess=readline(prompt = 'Guess: ')
 }
+print("Correct!")
 
 
   

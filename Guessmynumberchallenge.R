7d98c243ef88f380e87edad3c757fd08cde191a4
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-10-04 12:14:47 -0400

    Debugged

--- Guessmynumberchallenge.R
+++ Guessmynumberchallenge.R
@@ -1,26 +1,24 @@
+rm(list=ls())
+
 #make a vector of numbers from 1 to 100
 v=1:100
 #take input from the user-guess
-guess=readline(prompt = 'Guess: ')
+guess=100000
 #thinking of a number
 number= sample(v,size = 1,replace = FALSE, prob = NULL)
 
-while (guess == number){print("Correct"); break}
-while (guess < number){print("Higher")
-  guess=readline(prompt = 'Guess: ')}
-
-while (guess > number){print("Lower")
-  guess=readline(prompt = 'Guess: ')}
-
+print("I'm thinking of a number 1-100...")
 
 while(guess != number){
-  if(guess < number){
+  if (guess == 100000){
+    print("Make a guess")
+  }
+  else if(guess < number){
      print("Higher!")
   } else{
   print("Lower!")}
   guess=readline(prompt = 'Guess: ')
 }
-if (guess == number){print("Correct"); break}
 
-while (guess == number){print("Correct"); break}
+
   
